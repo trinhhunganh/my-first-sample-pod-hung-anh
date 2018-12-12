911f8790c9fc1c1a60d408b1efadd966a1725c42
anh-trinh <anh-trinh@cybozu.vn>
2018-12-12 10:29:15 +0700

    add some test source file

---
+++ my-first-sample-pod-hung-anh/Classes/Auth/my_auth.swift
@@ -0,0 +1,9 @@
+//
+//  my_auth.swift
+//  my-first-sample-pod-hung-anh
+//
+//  Created by Trinh Hung Anh on 12/12/18.
+//  Copyright © 2018 Trinh Hung Anh. All rights reserved.
+//
+
+import Foundation
